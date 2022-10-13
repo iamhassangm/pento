0f01d379166fc259eec1fb9934d3f0c94972da76
Hassan Gul <wedesignerdxb@gmail.com>
2022-10-13 11:15:44 +0500

    optimize: don't fetch user if it already exists using assign_new

--- lib/pento_web/live/user_auth_live.ex
+++ lib/pento_web/live/user_auth_live.ex
@@ -2,16 +2,19 @@ defmodule PentoWeb.UserAuthLive do
     import Phoenix.LiveView
     alias Pento.Accounts
 
-    def on_mount(conn, params, %{"user_token" => user_token}, socket) do
-        IO.inspect(conn, label: "CONN: ")
-        user = Accounts.get_user_by_session_token(user_token)
+    def on_mount(_, params, %{"user_token" => user_token}, socket) do
+        fetch_user = &Accounts.get_user_by_session_token(&1)
         socket =
             socket
-            |> assign(:current_user, user)
+            |> assign_new(:current_user, fn -> fetch_user.(user_token) end )
         if socket.assigns.current_user do
             {:cont, socket}
         else
             {:halt, redirect(socket, to: "/login")}
         end
+    end
+
+    def fetch_user(user_token) do
+        Accounts.get_user_by_session_token(user_token)
     end
 end
